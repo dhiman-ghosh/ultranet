a9981cbc38ebd2869c1555602e863c4f1636f069
Dhiman Ghosh <vicky.wb@gmail.com>
2019-06-14 18:54:00 +0530

    OTA firmware update support

--- Arduino/ultranet/ultranet.ino
+++ Arduino/ultranet/ultranet.ino
@@ -7,12 +7,14 @@ extern PubSubClient mqtt;
 void setup() {
   Serial.begin(9600);
   Serial.println("Setting up Wifi and Internet...");        setup_wifi();
+  Serial.println("Setting up OTA firmware update...");      setup_ota();
   Serial.println("Setting up MQTT Communication...");       setup_mqtt();
   Serial.println("Setting up RGB Sensor (TCS34725)...");    setup_rgb_sensor();
   Serial.println("Setting up NeoPixel (WS2812B)...");       setup_neopixel();
 }
 
 void loop() {
-  loop_mqtt();
+  loop_mqtt(); /* handles (a) arq, (b) neopixel */
+  loop_ota();
   EVERY_N_MILLISECONDS(250) { rgb_read_and_display(); }
 }
